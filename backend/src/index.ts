4bf7761a45d943278e928b12aa3e84af79579dfc
habul17 <abulhassanca.dev@gmail.com>
2026-08-16 14:47:12 +0530

    Update backend dependencies and dev script

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from 'express'
+import dotenv from 'dotenv'
 
 dotenv.config();
 
@@ -7,14 +7,12 @@ const app = express();
 
 
 app.get("/health", (req, res) => {
-
     res.json({
-        status : "ok"
+        status: "ok"
     })
-
 })
 
 
-app.listen(process.env.PORT || 3000, () => {
-     console.log(`server is running on port ${process.env.PORT || 3000}`);
-});
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`Server Running On Port ${process.env.PORT || 4000}`);
+})
